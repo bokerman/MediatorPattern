a3be332dfa6ceccdd41b00dcae9bf05fb4205a47
Bojan Ruzic <bokerman@gmail.com>
2020-06-05 22:50:57 -0400

    Initial commit.

--- MediatorPattern/Program.cs
+++ MediatorPattern/Program.cs
@@ -6,7 +6,7 @@ namespace MediatorPattern
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!"); 
         }
     }
 }
